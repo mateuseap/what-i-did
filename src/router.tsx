2fef74dfa355e5b4875b624522df048576598b96
Mateus Elias <meap@cin.ufpe.br>
2023-12-07 03:18:34 -0300

    fix(src/router.tsx): set BrowserRouter basename

--- src/router.tsx
+++ src/router.tsx
@@ -3,9 +3,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import MemberPage from "./pages/MemberPage";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/members/:id", element: <MemberPage /> },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <Home /> },
+    { path: "/members/:id", element: <MemberPage /> },
+  ],
+  { basename: "/what-i-did/" }
+);
 
 export default router;
